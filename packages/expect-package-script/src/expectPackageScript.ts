a6e7e1c9ccedfc38da13656146d952520df5561d
Eric Anderson <e@ericlanderson.com>
2018-12-18 13:15:43 -0500

    Adds expectPackageScript

---
+++ packages/expect-package-script/src/expectPackageScript.ts
@@ -0,0 +1,49 @@
+import { Context } from "@monorepo-lint/core";
+import { PackageJson } from "@monorepo-lint/utils";
+import * as fs from "fs";
+import diff from "jest-diff";
+
+export interface Opts {
+  name: string;
+  value: string;
+}
+
+export default function expectPackageScript(context: Context, opts: Opts) {
+  const packageJson = context.getPackageJson();
+  if (packageJson.scripts === undefined) {
+    context.addError({
+      file: context.getPackageJsonPath(),
+      message: "No scripts block in package.json",
+      fixer: () => {
+        mutateJson<PackageJson>(context.getPackageJsonPath(), input => {
+          input.scripts = {};
+          return input;
+        });
+      }
+    });
+    return;
+  }
+
+  if (packageJson.scripts[opts.name] !== opts.value) {
+    context.addError({
+      file: context.getPackageJsonPath(),
+      message: `Expected standardized script entry for '${opts.name}'`,
+      longMessage: diff(
+        opts.value + "\n",
+        (packageJson.scripts[opts.name] || "") + "\n"
+      ),
+      fixer: () => {
+        mutateJson<PackageJson>(context.getPackageJsonPath(), input => {
+          input.scripts![opts.name] = opts.value;
+          return input;
+        });
+      }
+    });
+  }
+}
+
+function mutateJson<T>(path: string, mutator: ((f: T) => T)) {
+  let file: T = JSON.parse(fs.readFileSync(path, "utf-8"));
+  file = mutator(file);
+  fs.writeFileSync(path, JSON.stringify(file, undefined, 2) + "\n");
+}
