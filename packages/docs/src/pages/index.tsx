a8c7bb48d08d58b34473764fca04cb602c63e411
Mac Lockard <maclockard@gmail.com>
2018-12-18 21:29:57 -0800

    Added new package for docs

---
+++ packages/docs/src/pages/index.tsx
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2018 monorepo-lint (http://monorepo-lint.com). All Right Reserved.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ *
+ */
+import { Link } from "gatsby";
+import React from "react";
+
+export default function() {
+  return (
+    <div>
+      <h1>monorepo-lint</h1>
+      <p>In development</p>
+      <Link to={"./quick-start"}>Quick Start</Link>
+    </div>
+  );
+}
---
+++ packages/docs/src/pages/quickStart.md
@@ -0,0 +1,11 @@
+---
+path: "/quick-start"
+title: "Quick Start"
+---
+
+## Code blocks
+
+```javascript
+var s = "JavaScript syntax highlighting";
+alert(s);
+```
