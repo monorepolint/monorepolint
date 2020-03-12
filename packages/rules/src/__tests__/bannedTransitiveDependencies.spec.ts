7530a9c3d2953a1805d9b68c292b9cb6a9f351f0
John Wiseheart <jwiseheart@palantir.com>
2020-03-12 09:43:25 -0700

    Fix comment

--- packages/rules/src/__tests__/bannedTransitiveDependencies.spec.ts
+++ packages/rules/src/__tests__/bannedTransitiveDependencies.spec.ts
@@ -1,11 +1,4 @@
 /*!
- * Copyright 2019 Palantir Technologies, Inc.
- *
- * Licensed under the MIT license. See LICENSE file in the project root for details.
- *
- */
-
-/*!
  * Copyright 2020 Palantir Technologies, Inc.
  *
  * Licensed under the MIT license. See LICENSE file in the project root for details.
