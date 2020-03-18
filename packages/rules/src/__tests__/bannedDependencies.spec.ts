71777320302871c818beb28cabec5b9fd5844e28
John Wiseheart <jwiseheart@palantir.com>
2020-03-18 11:30:57 -0700

    [Rule] Ban transitive dependencies (#112)
    * Banned transitive dependencies
    
    * Fix header
    
    * Fixes
    
    * Fix tslint config
    
    * Fix comment
    
    * Combine rules, use graph search instead

---
+++ packages/rules/src/__tests__/bannedDependencies.spec.ts
@@ -0,0 +1,96 @@
+/*!
+ * Copyright 2020 Palantir Technologies, Inc.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ *
+ */
+import { WorkspaceContext } from "@monorepolint/core";
+import { writeFileSync } from "fs";
+import * as path from "path";
+import * as tmp from "tmp";
+import { bannedDependencies } from "../bannedDependencies";
+import { makeDirectoryRecursively } from "../util/makeDirectory";
+import { jsonToString } from "./utils";
+
+const EMPTY_PACKAGE = jsonToString({});
+
+describe("bannedDependencies", () => {
+  tmp.setGracefulCleanup();
+
+  let cleanupJobs: Array<() => void> = [];
+  let cwd: string | undefined;
+
+  beforeEach(() => {
+    const dir = tmp.dirSync();
+    cleanupJobs.push(() => dir.removeCallback());
+    cwd = dir.name;
+
+    const spy = jest.spyOn(process, "cwd");
+    spy.mockReturnValue(cwd);
+  });
+
+  afterEach(() => {
+    for (const cleanupJob of cleanupJobs) {
+      cleanupJob();
+    }
+    cleanupJobs = [];
+  });
+
+  function makeWorkspace() {
+    const workspaceContext = new WorkspaceContext(cwd!, {
+      rules: [],
+      fix: false,
+      verbose: false,
+      silent: true,
+    });
+
+    async function checkAndSpy(bannedTransitives: string[]) {
+      const addErrorSpy = jest.spyOn(workspaceContext, "addError");
+      bannedDependencies.check(workspaceContext, {
+        bannedTransitiveDependencies: bannedTransitives,
+      });
+      return { addErrorSpy };
+    }
+
+    function addFile(filePath: string, content: string) {
+      const dirPath = path.resolve(cwd!, path.dirname(filePath));
+      const resolvedFilePath = path.resolve(cwd!, filePath);
+
+      makeDirectoryRecursively(dirPath);
+      writeFileSync(resolvedFilePath, content);
+      return resolvedFilePath;
+    }
+
+    return { addFile, workspaceContext, checkAndSpy };
+  }
+
+  // TODO: Add test for regular banned dependencies
+
+  it("Flags banned transitives correctly", async () => {
+    const { addFile, checkAndSpy } = makeWorkspace();
+    const rootPackageJson = jsonToString({
+      dependencies: {
+        aaa: "0.0.1",
+      },
+    });
+    addFile("./package.json", rootPackageJson);
+
+    const aaaPackageJson = jsonToString({
+      dependencies: {
+        bbb: "0.0.1",
+        ccc: "0.0.1",
+      },
+    });
+    addFile("./aaa/package.json", aaaPackageJson);
+    const bbbPackageJson = jsonToString({
+      dependencies: {
+        ddd: "0.0.1",
+      },
+    });
+    addFile("./aaa/bbb/package.json", bbbPackageJson);
+    addFile("./aaa/bbb/ddd/package.json", EMPTY_PACKAGE);
+    addFile("./aaa/ccc/package.json", EMPTY_PACKAGE);
+
+    expect((await checkAndSpy(["ccc", "ddd"])).addErrorSpy).toHaveBeenCalledTimes(2);
+  });
+});
