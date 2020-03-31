661c8aaaf03593209e7bb163da24f64bc73da7d3
Eric Anderson <e@ericlanderson.com>
2020-03-31 11:43:05 -0400

    Fix semantic merge conflict

--- packages/rules/src/__tests__/bannedDependencies.spec.ts
+++ packages/rules/src/__tests__/bannedDependencies.spec.ts
@@ -5,11 +5,10 @@
  *
  */
 import { WorkspaceContext } from "@monorepolint/core";
-import { writeFileSync } from "fs";
+import { NormalFileSystem } from "@monorepolint/utils";
 import * as path from "path";
 import * as tmp from "tmp";
 import { bannedDependencies } from "../bannedDependencies";
-import { makeDirectoryRecursively } from "../util/makeDirectory";
 import { jsonToString } from "./utils";
 
 const EMPTY_PACKAGE = jsonToString({});
@@ -37,12 +36,17 @@ describe("bannedDependencies", () => {
   });
 
   function makeWorkspace() {
-    const workspaceContext = new WorkspaceContext(cwd!, {
-      rules: [],
-      fix: false,
-      verbose: false,
-      silent: true,
-    });
+    const fs = new NormalFileSystem();
+    const workspaceContext = new WorkspaceContext(
+      cwd!,
+      {
+        rules: [],
+        fix: false,
+        verbose: false,
+        silent: true,
+      },
+      fs
+    );
 
     async function checkAndSpy(bannedTransitives: string[]) {
       const addErrorSpy = jest.spyOn(workspaceContext, "addError");
@@ -56,8 +60,8 @@ describe("bannedDependencies", () => {
       const dirPath = path.resolve(cwd!, path.dirname(filePath));
       const resolvedFilePath = path.resolve(cwd!, filePath);
 
-      makeDirectoryRecursively(dirPath);
-      writeFileSync(resolvedFilePath, content);
+      fs.mkdir(dirPath, { recursive: true });
+      fs.writeFile(resolvedFilePath, content);
       return resolvedFilePath;
     }
 
