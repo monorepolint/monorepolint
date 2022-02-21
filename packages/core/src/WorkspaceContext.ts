52615b6874f0d3d7a1dfa64882ed848db4180d35
Peter Goldberg <goldberg447@gmail.com>
2022-02-21 14:17:24 -0500

    make private

--- packages/core/src/WorkspaceContext.ts
+++ packages/core/src/WorkspaceContext.ts
@@ -12,7 +12,7 @@ import { PackageContext } from "./PackageContext";
 
 // Right now, this stuff is done serially so we are writing less code to support that. Later we may want to redo this.
 export class WorkspaceContext extends PackageContext {
-  public packageNameToDir: Map<string, string> | undefined;
+  private packageNameToDir: Map<string, string> | undefined;
   constructor(packageDir: string, opts: ResolvedConfig, parent?: Context) {
     super(packageDir, opts, parent);
   }
