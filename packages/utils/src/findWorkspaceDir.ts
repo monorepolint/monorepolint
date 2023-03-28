7fed7e1159e016196d526fef165e6706677029d9
David Schlosnagle <davids@palantir.com>
2023-03-28 17:40:10 -0400

    Less async import

--- packages/utils/src/findWorkspaceDir.ts
+++ packages/utils/src/findWorkspaceDir.ts
@@ -5,17 +5,16 @@
  *
  */
 
+import { dirname, join, normalize } from "path";
+import { promises } from "fs";
 import { Host } from "./Host.js";
 import { PackageJson } from "./PackageJson.js";
 
 export async function findPnpmWorkspaceDir(dir: string) {
-  const dirname = import("path").then((path) => path.dirname);
-  const workspaceManifestLocation = await import("fs").then((fs) =>
-    import("find-up").then((find) =>
-      fs.promises.realpath(dir).then((cwd) => find.findUp("pnpm-workspace.yaml", { cwd }))
-    )
+  const workspaceManifestLocation = await import("find-up").then((find) =>
+    promises.realpath(dir).then((cwd) => find.findUp("pnpm-workspace.yaml", { cwd }))
   );
-  return workspaceManifestLocation && (await dirname)(workspaceManifestLocation);
+  return workspaceManifestLocation && dirname(workspaceManifestLocation);
 }
 
 export async function findWorkspaceDir(
@@ -23,14 +22,13 @@ export async function findWorkspaceDir(
   dir: string
 ): Promise<string | undefined> {
   // Defining workspaces in package.json is not necessary in PNPM
-  const path = await import("path");
   const maybePnpmWorkspaceDir = await findPnpmWorkspaceDir(dir);
   if (maybePnpmWorkspaceDir != null) {
     return maybePnpmWorkspaceDir;
   }
 
   // We may not be in a repository that uses PNPM, look for workspaces in package.json
-  const packagePath = path.join(dir, "package.json");
+  const packagePath = join(dir, "package.json");
   if (host.exists(packagePath)) {
     const packageJson = host.readJson(packagePath) as PackageJson;
     if (packageJson.workspaces !== undefined) {
@@ -38,7 +36,7 @@ export async function findWorkspaceDir(
     }
   }
 
-  const nextDir = path.normalize(path.join(dir, ".."));
+  const nextDir = normalize(join(dir, ".."));
   if (nextDir === dir) {
     return undefined;
   }
