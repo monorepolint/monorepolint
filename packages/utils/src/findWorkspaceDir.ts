251b079dd13b180865e70c0784df51480d0d52c2
David Schlosnagle <davids@palantir.com>
2023-03-28 17:08:54 -0400

    Extra lazy imports

--- packages/utils/src/findWorkspaceDir.ts
+++ packages/utils/src/findWorkspaceDir.ts
@@ -5,17 +5,17 @@
  *
  */
 
-import { dirname, join, normalize } from "path";
 import { Host } from "./Host.js";
 import { PackageJson } from "./PackageJson.js";
 
-export async function findPnpmWorkspaceDir(cwd: string) {
-  const { findUp } = await import("find-up");
-  const { promises } = await import("fs");
-  const workspaceManifestLocation = await findUp("pnpm-workspace.yaml", {
-    cwd: await promises.realpath(cwd),
-  });
-  return workspaceManifestLocation && dirname(workspaceManifestLocation);
+export async function findPnpmWorkspaceDir(dir: string) {
+  const dirname = import("path").then((path) => path.dirname);
+  const workspaceManifestLocation = await import("fs").then((fs) =>
+    import("find-up").then((find) =>
+      fs.promises.realpath(dir).then((cwd) => find.findUp("pnpm-workspace.yaml", { cwd }))
+    )
+  );
+  return workspaceManifestLocation && (await dirname)(workspaceManifestLocation);
 }
 
 export async function findWorkspaceDir(
@@ -23,13 +23,14 @@ export async function findWorkspaceDir(
   dir: string
 ): Promise<string | undefined> {
   // Defining workspaces in package.json is not necessary in PNPM
+  const path = await import("path");
   const maybePnpmWorkspaceDir = await findPnpmWorkspaceDir(dir);
   if (maybePnpmWorkspaceDir != null) {
     return maybePnpmWorkspaceDir;
   }
 
   // We may not be in a repository that uses PNPM, look for workspaces in package.json
-  const packagePath = join(dir, "package.json");
+  const packagePath = path.join(dir, "package.json");
   if (host.exists(packagePath)) {
     const packageJson = host.readJson(packagePath) as PackageJson;
     if (packageJson.workspaces !== undefined) {
@@ -37,7 +38,7 @@ export async function findWorkspaceDir(
     }
   }
 
-  const nextDir = normalize(join(dir, ".."));
+  const nextDir = path.normalize(path.join(dir, ".."));
   if (nextDir === dir) {
     return undefined;
   }
